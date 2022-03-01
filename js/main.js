5926e771121a1d5c8bb75e63fe178f67ca4ae241
jeongeum <wjddma1202@naver.com>
2022-03-01 16:29:40 +0900

    0301 update
    수정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const userLogin = document.querySelector(".user_login");
 const userInput = document.querySelector(".user_login input");
 const userBtn = document.querySelector(".user_btn");
-const savedUsername = localStorage.getItem("username"); // 로컬스토리지 저장 여부
+const savedUsername = localStorage.getItem("user"); // 로컬스토리지 저장 여부
 
 const USERNAME_KEY = "user";
 
@@ -31,12 +31,11 @@ function saveName(username) {
   //nextPage();
 }
 
-function nextPage() {
-  document.location.href = "main/index.html";
-}
-
 if (savedUsername === null) {
   send();
 } else {
   nextPage();
+}
+function nextPage() {
+  document.location.href = "main/index.html";
 }
