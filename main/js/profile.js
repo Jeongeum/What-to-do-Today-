a0490b485f672f18967716284c71d5dc0f85351c
jeongeum <wjddma1202@naver.com>
2022-03-02 00:44:33 +0900

    0301 user profileImg change
    유저의 프로필 사진 변경 가능. but 새로고침 시 사라짐

--- main/js/profile.js
+++ main/js/profile.js
@@ -1,14 +1,19 @@
 const user = document.querySelector(".user");
 const usernameInput = document.querySelector(".profileName_input");
-const savedUsername = localStorage.getItem("user");
 
+const profileBtn = document.querySelector(".profileImg_btn");
+const profileInput = document.querySelector(".profile_img");
+const profileIcon = document.querySelector(".fa-user-plus");
+
+const savedUsername = localStorage.getItem("user");
 const USERNAME_KEY = "user";
+const PROFILE_IMG = "userImg";
 // const HIDDEN_CLASSNAME = "hidden";
 // const HIDE = "hide";
 // const SHOW = "show";
 
+/* 이름 변경 */
 function handleUserNameInput() {
-  console.log("클릭!");
   user.classList.add(HIDE);
   usernameInput.classList.add(SHOW);
   changeName();
@@ -27,4 +32,35 @@ function changeName() {
     }
   });
 }
+
+/* 프로필 사진 변경 */
+profileBtn.addEventListener("click", () => {
+  profileInput.click();
+});
+
+function handleProfileImg() {
+  if (profileBtn.children.length > 0) {
+    // 프로필 사진이 두개이면 처음 사진을 지운다.
+    localStorage.removeItem(PROFILE_IMG);
+    profileBtn.children[0].remove();
+    console.log(profileBtn.children);
+  }
+
+  const reader = new FileReader();
+  reader.onload = () => {
+    localStorage.setItem(PROFILE_IMG, reader.result);
+    loadImgUrl(reader.result);
+  };
+
+  reader.readAsDataURL(profileInput.files[0]);
+}
+
+function loadImgUrl(DataUrl) {
+  const image = document.createElement("img");
+  profileIcon.remove();
+  profileBtn.appendChild(image);
+  image.src = DataUrl;
+}
+
+profileInput.addEventListener("change", handleProfileImg);
 user.addEventListener("click", handleUserNameInput);
