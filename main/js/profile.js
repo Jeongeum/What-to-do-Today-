e8fe0645f2b20e0ddccd860f490f7b4d6bec8257
jeongeum <wjddma1202@naver.com>
2022-03-03 00:53:52 +0900

    0302 프로필 사진 새로고침 이슈 미해결

--- main/js/profile.js
+++ main/js/profile.js
@@ -4,8 +4,19 @@ const usernameInput = document.querySelector(".profileName_input");
 const profileBtn = document.querySelector(".profileImg_btn");
 const profileInput = document.querySelector(".profile_img");
 const profileIcon = document.querySelector(".fa-user-plus");
+const fileTypes = [
+  "image/apng",
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/tiff",
+  "image/webp",
+  "image/x-icon",
+];
 
-const savedUsername = localStorage.getItem("user");
 const USERNAME_KEY = "user";
 const PROFILE_IMG = "userImg";
 // const HIDDEN_CLASSNAME = "hidden";
@@ -39,20 +50,42 @@ profileBtn.addEventListener("click", () => {
 });
 
 function handleProfileImg() {
+  const currentFiles = profileInput.files;
+
   if (profileBtn.children.length > 0) {
     // 프로필 사진이 두개이면 처음 사진을 지운다.
     localStorage.removeItem(PROFILE_IMG);
     profileBtn.children[0].remove();
-    console.log(profileBtn.children);
   }
 
-  const reader = new FileReader();
-  reader.onload = () => {
-    localStorage.setItem(PROFILE_IMG, reader.result);
-    loadImgUrl(reader.result);
-  };
+  //   if (profileInput.files[0]) {
+  //     const reader = new FileReader();
+  //     reader.onload = () => {
+  //       localStorage.setItem(PROFILE_IMG, reader.result);
+  //       loadImgUrl(reader.result);
+  //     };
 
-  reader.readAsDataURL(profileInput.files[0]);
+  //     reader.readAsDataURL(profileInput.files[0]);
+  //   }
+
+  if (currentFiles.length === 0) {
+    alert("No files currently selected for upload");
+  } else {
+    // for of 는 배열
+    for (const file of currentFiles) {
+      if (validFileType(file)) {
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+          localStorage.setItem(PROFILE_IMG, reader.result);
+          loadImgUrl(reader.result);
+          console.log(file);
+        });
+        reader.readAsDataURL(file);
+      } else {
+        alert("Not a valid file type. Update your selection.");
+      }
+    }
+  }
 }
 
 function loadImgUrl(DataUrl) {
@@ -60,6 +93,10 @@ function loadImgUrl(DataUrl) {
   profileIcon.remove();
   profileBtn.appendChild(image);
   image.src = DataUrl;
+}
+
+function validFileType(file) {
+  return fileTypes.includes(file.type);
 }
 
 profileInput.addEventListener("change", handleProfileImg);
